1b008453adf164334515f8d29c9f4196b4f9ae37
Saurav Kumar <95sauravkumar95@gmail.com>
2026-02-21 20:08:33 +0530

    feat(client): add CodeInsights route to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CPTracker from './pages/CPTracker';
+import CodeInsights from './pages/CodeInsights';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cp-tracker" element={<CPTracker />} />
+        <Route path="/code-insights" element={<CodeInsights />} />
         {/* Module Routes will go here */}
       </Routes>
     </Router>
